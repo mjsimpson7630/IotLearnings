f16776a0db56e8a6026d8ee1cfdfcfd69af9d02f
Michael Simpson <msimpson@actia.com>
2018-03-16 15:51:41 -0400

    template F0 project for Eclipse, from Mastering STM32

---
+++ nucleo-f0/nucleo-f0/Debug/src/subdir.mk
@@ -0,0 +1,36 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../src/_write.c \
+../src/main.c \
+../src/stm32f0xx_hal_msp.c \
+../src/stm32f0xx_it.c \
+../src/system_stm32f0xx.c 
+
+OBJS += \
+./src/_write.o \
+./src/main.o \
+./src/stm32f0xx_hal_msp.o \
+./src/stm32f0xx_it.o \
+./src/system_stm32f0xx.o 
+
+C_DEPS += \
+./src/_write.d \
+./src/main.d \
+./src/stm32f0xx_hal_msp.d \
+./src/stm32f0xx_it.d \
+./src/system_stm32f0xx.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross ARM GNU C Compiler'
+	arm-none-eabi-gcc -mcpu=cortex-m0 -mthumb -Og -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -ffreestanding -fno-move-loop-invariants -Wall -Wextra  -g3 -DDEBUG -DSTM32F030x8 -DTRACE -DOS_USE_TRACE_SEMIHOSTING_DEBUG -I"../include" -I"../system/include" -I"../system/include/cmsis" -I"../system/include/stm32f0xx" -std=gnu11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
